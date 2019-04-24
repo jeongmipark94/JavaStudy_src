63d297ca3081af6fbe982382c1a89e53686dad97
jeongmipark94 <45455409+jeongmipark94@users.noreply.github.com>
2019-04-24 14:25:44 +0900

    0424

---
+++ spring_jdbc/src/kr/co/sist/dao/jdbcDAO.java
@@ -0,0 +1,16 @@
+package kr.co.sist.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+@Component
+public class jdbcDAO {
+	@Autowired(required=false)
+	private JdbcTemplate jt;
+	
+	public JdbcTemplate getJt() throws DataAccessException {
+		return jt;
+	}//getJt
+
+}//class
---
+++ spring_jdbc/src/kr/co/sist/service/JdbcService.java
@@ -0,0 +1,18 @@
+package kr.co.sist.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import kr.co.sist.dao.jdbcDAO;
+
+@Component
+public class JdbcService {
+		@Autowired(required=false)
+		private jdbcDAO jdao;
+	
+		public void daoPrint() {
+			System.out.println(" Äõ¸®½ÇÇà °´Ã¼(JDBC Template) : "+ jdao.getJt());
+		}
+		
+	
+}
